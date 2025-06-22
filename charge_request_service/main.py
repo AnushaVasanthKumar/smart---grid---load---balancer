b8af31dcb0773fb14c49f20625ac7fed011cb16c
AnushaVasanthKumar <anushavasanthkumar95@gmail.com>
2025-06-22 20:43:45 +0530

    Add files via upload

--- charge_request_service/main.py
+++ charge_request_service/main.py
@@ -1,1 +1,18 @@
+from flask import Flask, request
+import requests
+
+app = Flask(__name__)
+
+LOAD_BALANCER_URL = "http://load_balancer:5000/request_charge"
+
+@app.route('/charge', methods=['POST'])
+def handle_request():
+    try:
+        res = requests.post(LOAD_BALANCER_URL)
+        return res.text, res.status_code
+    except:
+        return "Failed to reach load balancer", 500
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
 
